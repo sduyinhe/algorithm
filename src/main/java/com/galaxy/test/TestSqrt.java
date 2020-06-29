4a732f43d8d8e6ce4bb3af4539f262cb0fc204f6
yanghw <yanghw@cheche365.com>
2020-06-29 10:28:12 +0800

    feat(test): test commit

--- src/main/java/com/galaxy/test/TestSqrt.java
+++ src/main/java/com/galaxy/test/TestSqrt.java
@@ -1,4 +1,8 @@
 package com.galaxy.test;
 
 public class TestSqrt {
+    public static void main(String[] args) {
+        double caculate = 7200D;
+        System.out.println("平方根为"+Math.sqrt(caculate));
+    }
 }
