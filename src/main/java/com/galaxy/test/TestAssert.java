2c507e8ac8e329e68b956042cd81c65a7923525e
yanghw <yanghw@cheche365.com>
2020-07-01 14:35:24 +0800

    feat(test): some test

---
+++ src/main/java/com/galaxy/test/TestAssert.java
@@ -0,0 +1,8 @@
+package com.galaxy.test;
+
+public class TestAssert {
+    public static void main(String[] args) {
+        assert false:"断言无法通过啊";
+        System.out.println("断言通过！");
+    }
+}
---
+++ src/main/java/com/galaxy/test/TestSensitive.java
@@ -0,0 +1,4 @@
+package com.galaxy.test;
+
+public class TestSensitive {
+}
